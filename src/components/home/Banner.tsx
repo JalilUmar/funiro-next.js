90a5a24f7288c7ab7146bdf72bf0696e2e0e3520
Jalil Umar <jalil786.umer@gmail.com>
2023-08-25 12:24:56 +0500

    fixing cart

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -10,11 +10,11 @@ export default function Banner() {
             <section className="w-1/2 ml-[180px] pl-[100px]">
                 <h2 className="text-orange-800 font-sans font-extrabold text-7xl">Best <br /> Furniture <br /> Designz</h2>
                 <br />
-                <p className="text-red-800 font-sans font-bold text-base">If you are going to use a passage of Lorem Ipsum, you need to be <br /> sure there isn't anything embarrassing hidden in the middle of text.</p>
-                
-            <Link href={'/our-designs'} >
-                <button className="flex gap-x-3 text-2xl font-sans font-bold text-orange-900 bg-transparent border-[1px] rounded-sm border-orange-900 px-6 py-3 mt-10 hover:bg-orange-900 hover:text-white hover:scale-105 active:scale-100  transition-all" >Shop Now <BsShop className="mt-1" /> </button>
-            </Link>
+                <p className="text-red-800 font-sans font-bold text-base">If you are going to use a passage of Lorem Ipsum, you need to be <br /> sure there isnt anything embarrassing hidden in the middle of text.</p>
+
+                <Link href={'/our-designs'} >
+                    <button className="flex gap-x-3 text-2xl font-sans font-bold text-orange-900 bg-transparent border-[1px] rounded-sm border-orange-900 px-6 py-3 mt-10 hover:bg-orange-900 hover:text-white hover:scale-105 active:scale-100  transition-all" >Shop Now <BsShop className="mt-1" /> </button>
+                </Link>
 
             </section>
 
